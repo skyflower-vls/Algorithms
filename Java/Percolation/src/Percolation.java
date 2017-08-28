6dc586d941438dd4573c109fa38cb09dd077ebf1
skyflower-vls <skyflower-vls@users.noreply.github.com>
2017-08-28 11:41:42 +0300

    fixed

--- Java/Percolation/src/Percolation.java
+++ Java/Percolation/src/Percolation.java
@@ -1,178 +1,234 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-//temp
-import java.util.concurrent.ThreadLocalRandom;
 
-// NEED to connect first virtual el to first row and  last column to bottom virtual, virtual bottom connect to virtual top
+//temp for testing
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Percolation {
 	
 	private boolean[][] _field;
 	private WeightedQuickUnionUF _quickUnion;
-	private int _topRoot = 0;
-	private int _bottomRoot = 0;
-	private int _fieldSize = 0;
-	private int _numInRow = 0;
+	private int _virtualTopRoot = 0;
+	private int _virtualBottomRoot = 0;
+	private int _fieldArrayNumberOfElements = 0;
+	private int _size = 0;
 	private int _openedElementsCount = 0;
 	
-	   public Percolation(int n)                // create n-by-n grid, with all sites blocked
-	   {
-		   
-		   _numInRow = n;
-		   _fieldSize = n * n;
-		   _bottomRoot = _fieldSize + 1;
-		   
-		   int corr = _fieldSize + 2;
-			_quickUnion = new WeightedQuickUnionUF(corr); // plus two virtual ids
-			//_quickUnion.union(_topRoot, _bottomRoot);
-			
-			createField(n);
-			
-				
-			showArray();
-			randomOpenField();
-			System.out.println("__");
-			if (percolates())
-			{
-				System.out.println("Hello, World");
-				
-			}
-			showArray();	
-			
-			
-			
-			
-	   }
+	/**
+	 * create n-by-n grid, with all sites blocked
+	 * @param n
+	 */
+	public Percolation(int n)
+	{	   
+	   _size = n;
+	   _fieldArrayNumberOfElements = n * n;
 	   
-	   private void randomOpenField()
-	   {
-		   for (int i = 0; i < _numInRow; i++)
-		   {
-			   for (int y = 0; y < _numInRow; y ++)
-			   {
-				  if (_field[i][y]) {
-					  open(i, y);
-				  }
-			   }
-		   }
+	   _virtualTopRoot = 0;
+	   _virtualBottomRoot = _fieldArrayNumberOfElements + 1;
+	   
+	   int fixedArraySizeWithVirtualElements = _fieldArrayNumberOfElements + 2;  // plus two virtual ids
+		_quickUnion = new WeightedQuickUnionUF(fixedArraySizeWithVirtualElements);
+					
+		createField(n);
+		
+		// temp for testing
+		showArray();
+		
+		// temp for testing
+		randomOpenField();
+		
+		// temp for testing
+		System.out.println("__");
+		
+		// temp for testing
+		if (percolates())
+		{
+			System.out.println("Percolates!");			
 		}
-	   
-	   private int getIdNumberFromArray(int row, int col)
+		
+		// temp for testing
+		showArray();			
+	}
+   
+    /**
+     * test client (optional)
+     * @param args
+     */
+    public static void main(String[] args)   
+    {   
+    	// temp for testing
+	    new Percolation(5);
+    }
+   
+    /**
+     * is site (row, col) open?
+     * @param row
+     * @param col
+     * @return boolean
+     */
+    public boolean isOpen(int row, int col)
+    {
+	   if (!isFull(row, col)) 
 	   {
-		   int result = (_numInRow * row)  +  col + 1; //plus first virtual
-		   
-		   return result;
+		   return _field[row][col];
 	   }
-	   
-	   private void createField(int size)
+	   else
 	   {
-		   _field = new boolean[size][size];
-		   for (int i = 0; i < size; i++) 
-		   {
-			   for (int y = 0; y < size; y ++)
-			   {
-				   int tt = ThreadLocalRandom.current().nextInt(0, 2);
-					  if(tt == 0)
-					  {
-						  _field[i][y] = true;
-					  }
-					  else {
-						  
-						  _field[i][y] = false;
-					  }
-			   }
-		   }
+		   return false;
 	   }
-	   
-	   private void showArray()
+    }
+   
+    /**
+     * Is site (row, col) full? 
+     * @param row
+     * @param col
+     * @return boolean
+     */
+    public boolean isFull(int row, int col)  
+    {
+	   boolean result = (col >= _size || col < 0 || row < 0 || row >= _size);
+	   return result; 		
+    }
+   
+    /**
+     * number of open sites
+     * @return integer
+     */
+    public int numberOfOpenSites()       
+    {
+	   return _openedElementsCount;
+    }
+   
+	/**
+	* does the system percolate?
+	* @return boolean
+	*/
+   public boolean percolates()              
+   {
+	   return _quickUnion.connected(_virtualTopRoot, _virtualBottomRoot);
+   }
+   
+   /**
+    * open site (row, col) if it is not open already
+    * @param row
+    * @param col
+    */
+   public void open(int row, int col)    // 
+   {   
+	   if (!isOpen(row, col)) 
 	   {
-		   String res = "";
-		   for (int i = 0; i < _numInRow; i++) 
-		   {			 
-			   res = "";
-			   for (int y = 0; y < _numInRow; y++)
-			   {
-				   
-				   if( _field[i][y]) 
-					{
-						res = res.concat("1 ");
-					} 
-					else 
-					{
-						res = res.concat("0 ");
-					}   
-			   }
-				
-			   
-				System.out.println(res);
-		   }
-	   }
-	  	   
-	   
-	   public void open(int row, int col)    // open site (row, col) if it is not open already
-	   {
-		   //_field[row][col] = true;
+		   _field[row][col] = true;
 		   _openedElementsCount ++;
+		   
+		   int currentId = getIdNumberFromArray(row, col);
 		   
 		   if (row == 0) 
 		   {
-			   _quickUnion.union(getIdNumberFromArray(row, col), _topRoot); 
+			   _quickUnion.union(currentId, _virtualTopRoot); 
 		   }
-		   if (row == _numInRow - 1) 
+		   
+		   if (row == _size - 1) 
 		   {
-			   _quickUnion.union(getIdNumberFromArray(row, col), _bottomRoot); 
+			   _quickUnion.union(currentId, _virtualBottomRoot); 
+		   }   
+	
+		   if (isOpen(row-1, col)) 
+		   {				   
+			   _quickUnion.union(currentId, getIdNumberFromArray(row -  1, col));
 		   }
-			   
-
-		   if (isOpen(row-1, col)) {				   
-			   _quickUnion.union(getIdNumberFromArray(row, col), getIdNumberFromArray(row -  1, col));
-		   }
-		   if (isOpen(row+1, col)) {				   
-			   _quickUnion.union(getIdNumberFromArray(row, col), getIdNumberFromArray(row +  1, col));
+		   
+		   if (isOpen(row+1, col)) 
+		   {				   
+			   _quickUnion.union(currentId, getIdNumberFromArray(row +  1, col));
 		   }	
-		   if(isOpen(row, col + 1)) {
-			   _quickUnion.union(getIdNumberFromArray(row, col), getIdNumberFromArray(row, col + 1));
-		   }
-		   if(isOpen(row, col - 1)) {
-			   _quickUnion.union(getIdNumberFromArray(row, col), getIdNumberFromArray(row, col - 1));
-		   }
-			 
-	   }
-	   
-	   public boolean isOpen(int row, int col)  // is site (row, col) open?
-	   {
-		   if (!isFull(row, col)) {
-			   return _field[row][col];
-		   }
-		   else
+		   
+		   if (isOpen(row, col + 1)) 
 		   {
-			   return false;
+			   _quickUnion.union(currentId, getIdNumberFromArray(row, col + 1));
+		   }
+		   
+		   if (isOpen(row, col - 1)) 
+		   {
+			   _quickUnion.union(currentId, getIdNumberFromArray(row, col - 1));
 		   }
 	   }
+   }
+   
+   /**
+    * get position in the array (+virtual top id)
+    * @param row
+    * @param col
+    * @return integer
+    */
+   private int getIdNumberFromArray(int row, int col)
+   {
+	   int result = (_size * row)  +  col + 1; //plus first virtual
 	   
-	   public boolean isFull(int row, int col)  // is site (row, col) full?
-	   {
-		   boolean result = (col >= _numInRow || col < 0 || row < 0 || row >= _numInRow);
-		   return result; 
-		
-	   }
+	   return result;
+   }
+   
+   /**
+    * create array with default values with fixed size  - size * size
+    * @param size
+    */
+   private void createField(int size)
+   {
+	   _field = new boolean[size][size];
 	   
-	   public int numberOfOpenSites()       // number of open sites
+	   for (int i = 0; i < size; i++) 
 	   {
-		   return _openedElementsCount;
+		   for (int y = 0; y < size; y ++)
+		   {
+			   _field[i][y] = false;
+		   }
 	   }
+   }
+   
+   /**
+    * print array to the console
+    */
+   private void showArray()
+   {
+	   String result = "";
 	   
-	   public boolean percolates()              // does the system percolate?
-	   {
-		   return _quickUnion.connected(_topRoot, _bottomRoot);
-	   }
-
-	   public static void main(String[] args)   // test client (optional)
-	   {
+	   for (int i = 0; i < _size; i++) 
+	   {			 
+		   result = "";
 		   
+		   for (int y = 0; y < _size; y++)
+		   {
+			   
+			   if( _field[i][y]) 
+				{
+				   result = result.concat("1 ");
+				} 
+				else 
+				{
+					result = result.concat("0 ");
+				}   
+		   }				
 		   
-		   Percolation percolation = new Percolation(5);
-	   }		
-
+			System.out.println(result);
+	   }
+   }
+   
+   
+   /**
+    * optional for testing, fills array with random values
+    */
+   private void randomOpenField()
+   {
+	   for (int i = 0; i < _size; i++)
+	   {
+		   for (int y = 0; y < _size; y ++)
+		   {			 
+			  int random = ThreadLocalRandom.current().nextInt(0, 2);
+			  if(random == 1)
+			  {						  
+				  open(i, y);
+			  }
+		   }
+	   }
+	}
 }
