b7dba64bc30e39da00035ce5367a667177e692ce
skyflower-vls <yurchuk.vl@gmail.com>
2017-08-29 18:09:43 +0300

    preparation

---
+++ Java/Deques and Randomized Queues/src/Deque.java
@@ -0,0 +1,82 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+	/**
+	 * construct an empty deque
+	 */
+	public Deque()                           // 
+	{
+		   
+	}
+	   
+	/**
+	 * is the deque empty?
+	 * @return
+	 */
+	public boolean isEmpty()                 // 
+	{
+		return false;
+	}
+	   
+	/**
+	 * return the number of items on the deque
+	 * @return
+	 */
+	public int size()                        
+	{
+		return 0;
+	}
+	   
+	/**
+	 * add the item to the front
+	 * @param item
+	 */
+	public void addFirst(Item item)          
+	{
+		
+	}
+	
+	/**
+	 * add the item to the end
+	 * @param item
+	 */
+	public void addLast(Item item)           
+	{
+		
+	}
+	   
+	/**
+	 * remove and return the item from the front
+	 * @return
+	 */
+	public Item removeFirst()                
+	{
+		return null;
+	}
+	   
+	/**
+	 * remove and return the item from the end
+	 * @return
+	 */
+	public Item removeLast()                  
+	{
+		return null;
+	}
+	   
+	/**
+	 * return an iterator over items in order from front to end
+	 */
+	public Iterator<Item> iterator()         
+	{
+		return null;
+	}
+	   
+	/**
+	 * unit testing (optional)
+	 * @param args
+	 */
+	public static void main(String[] args)   
+	{
+		   
+	}
+}
---
+++ Java/Deques and Randomized Queues/src/RandomizedQueue.java
@@ -0,0 +1,72 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+	/**
+	 * construct an empty randomized queue
+	 */
+	public RandomizedQueue() 
+	{
+		
+	}
+
+	/**
+	 * is the queue empty?
+	 * @return
+	 */
+	public boolean isEmpty()
+	{
+		return false;
+	}
+	   
+	/**
+	 * return the number of items on the queue	 
+	 * @return
+	 */
+	public int size()                        
+	{
+		return 0;
+	}
+
+	/**
+	 * add the item
+	 * @param item
+	 */
+	public void enqueue(Item item)            
+	{
+		
+	}
+	 
+	/**
+	 * remove and return a random item
+	 * @return
+	 */
+	public Item dequeue()                    
+	{
+		return null;
+	}
+   
+	/**
+	 * return (but do not remove) a random item
+	 * @return
+	 */
+	public Item sample()                     
+	{
+		return null;
+	}
+	   
+	/**
+	 * return an independent iterator over items in random order
+	 */
+	public Iterator<Item> iterator()          
+	{
+		return null;
+	}
+	
+	/**
+	 * unit testing (optional)
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{		
+	}
+}
